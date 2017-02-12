b16bd06297b1b6efbc526877ca784687dd80f586
bucricket <bucricket@gmail.com>
2017-02-12 15:25:58 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
     from distutils.core import setup
     setup_kwargs = {'scripts': ['bin/processlst']}
     
-from preparepydisalexi import __version__
+from processlst import __version__
 
 #=====build DMS binaries===============================
 # get Anaconda root location
