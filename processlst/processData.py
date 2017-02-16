88590a9dd81a0584685b10ba6af4209adb5bf09b
bucricket <bucricket@gmail.com>
2017-02-16 17:31:58 -0500

    Updated pydap imports

--- processlst/processData.py
+++ processlst/processData.py
@@ -15,8 +15,8 @@ import shutil
 import glob
 from .landsatTools import landsat_metadata,GeoTIFF
 from .utils import folders,writeArray2Tiff,getHTTPdata
-from .pydap_fork import urs
-from .pydap_fork import client
+from processlst.pydap_fork import urs
+from processlst.pydap_fork import client
 
 
 
