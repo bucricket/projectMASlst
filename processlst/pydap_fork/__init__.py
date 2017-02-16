fa35976e731a3a05008db85533b6ffbc8fe6e51a
bucricket <bucricket@gmail.com>
2017-02-16 17:10:05 -0500

    Removed garbage from the __init__ file

--- processlst/pydap_fork/__init__.py
+++ processlst/pydap_fork/__init__.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 16 16:13:40 2017
-
-@author: mschull
-"""
 from .pydap.src.pydap.cas import urs
 from .pydap.src.pydap import client
 
