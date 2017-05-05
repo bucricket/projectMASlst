918d417cee5be142e3876cd444574611cbd7fe37
bucricket <bucricket@gmail.com>
2017-05-05 14:39:55 -0400

    Fixed path names

--- processlst/lndlst_dms.py
+++ processlst/lndlst_dms.py
@@ -20,22 +20,22 @@ import shutil
 
 base = os.getcwd()
 Folders = folders(base)   
-landsatSR = Folders['landsatSR']
-landsatTemp = Folders['landsatTemp']
-landsatLST = Folders['landsatLST']
+landsat_SR = Folders['landsat_SR']
+landsat_temp = Folders['landsat_Temp']
+landsat_LST = Folders['landsat_LST']
 # global prediction
 
 
 def perpareDMSinp(productID,s_row,s_col,locglob,ext):
-    meta = landsat_metadata(os.path.join(landsatTemp,'%s_MTL.txt' % productID))
+    meta = landsat_metadata(os.path.join(landsat_temp,'%s_MTL.txt' % productID))
     sceneID = meta.LANDSAT_SCENE_ID
-    blue = os.path.join(landsatTemp,"%s_sr_band2.blue.dat" % sceneID)
-    green = os.path.join(landsatTemp,"%s_sr_band3.green.dat" % sceneID)
-    red = os.path.join(landsatTemp,"%s_sr_band4.red.dat" % sceneID)
-    nir = os.path.join(landsatTemp,"%s_sr_band5.nir.dat" % sceneID)
-    swir1 = os.path.join(landsatTemp,"%s_sr_band6.swir1.dat" % sceneID)
-    swir2 = os.path.join(landsatTemp,"%s_sr_band7.swir2.dat" % sceneID)
-    cloud = os.path.join(landsatTemp,"%s_cfmask.cloud.dat" % sceneID)
+    blue = os.path.join(landsat_temp,"%s_sr_band2.blue.dat" % sceneID)
+    green = os.path.join(landsat_temp,"%s_sr_band3.green.dat" % sceneID)
+    red = os.path.join(landsat_temp,"%s_sr_band4.red.dat" % sceneID)
+    nir = os.path.join(landsat_temp,"%s_sr_band5.nir.dat" % sceneID)
+    swir1 = os.path.join(landsat_temp,"%s_sr_band6.swir1.dat" % sceneID)
+    swir2 = os.path.join(landsat_temp,"%s_sr_band7.swir2.dat" % sceneID)
+    cloud = os.path.join(landsat_temp,"%s_cfmask.cloud.dat" % sceneID)
     sw_res = meta.GRID_CELL_SIZE_REFLECTIVE
     ulx = meta.CORNER_UL_PROJECTION_X_PRODUCT-(sw_res*0.5)
     uly = meta.CORNER_UL_PROJECTION_Y_PRODUCT+(sw_res*0.5)
@@ -50,9 +50,9 @@ def perpareDMSinp(productID,s_row,s_col,locglob,ext):
     ncols = meta.REFLECTIVE_SAMPLES
     zone = meta.UTM_ZONE
     #filestem = os.path.join(landsatLAI,"lndsr_modlai_samples.combined_%s-%s" %(startDate,endDate))
-    lstFN = os.path.join(landsatTemp,"lndsr.%s.cband6.bin" % sceneID)
-    sharpendFN = os.path.join(landsatTemp,"%s.%s_sharpened_%d_%d.%s" % (sceneID,locglob,s_row,s_col,ext))
-    #fn = os.path.join(landsatTemp,"dms_%d_%d.inp" % (s_row,s_col))
+    lstFN = os.path.join(landsat_temp,"lndsr.%s.cband6.bin" % sceneID)
+    sharpendFN = os.path.join(landsat_temp,"%s.%s_sharpened_%d_%d.%s" % (sceneID,locglob,s_row,s_col,ext))
+    #fn = os.path.join(landsat_temp,"dms_%d_%d.inp" % (s_row,s_col))
     fn = "dms_%d_%d.inp" % (s_row,s_col)
     file = open(fn, "w")
     file.write("# input file for Data Mining Sharpener\n")
@@ -92,16 +92,16 @@ def perpareDMSinp(productID,s_row,s_col,locglob,ext):
     file.close()
 
 def finalDMSinp(productID,ext):
-    meta = landsat_metadata(os.path.join(landsatTemp,'%s_MTL.txt' % productID))
+    meta = landsat_metadata(os.path.join(landsat_temp,'%s_MTL.txt' % productID))
     sceneID = meta.LANDSAT_SCENE_ID
-    blue = os.path.join(landsatTemp,"%s_sr_band2.blue.dat" % sceneID)
-    green = os.path.join(landsatTemp,"%s_sr_band3.green.dat" % sceneID)
-    red = os.path.join(landsatTemp,"%s_sr_band4.red.dat" % sceneID)
-    nir = os.path.join(landsatTemp,"%s_sr_band5.nir.dat" % sceneID)
-    swir1 = os.path.join(landsatTemp,"%s_sr_band6.swir1.dat" % sceneID)
-    swir2 = os.path.join(landsatTemp,"%s_sr_band7.swir2.dat" % sceneID)
-    cloud = os.path.join(landsatTemp,"%s_cfmask.cloud.dat" % sceneID)
-    #meta = landsat_metadata(os.path.join(landsatTemp,'%s_MTL.txt' % sceneID))
+    blue = os.path.join(landsat_temp,"%s_sr_band2.blue.dat" % sceneID)
+    green = os.path.join(landsat_temp,"%s_sr_band3.green.dat" % sceneID)
+    red = os.path.join(landsat_temp,"%s_sr_band4.red.dat" % sceneID)
+    nir = os.path.join(landsat_temp,"%s_sr_band5.nir.dat" % sceneID)
+    swir1 = os.path.join(landsat_temp,"%s_sr_band6.swir1.dat" % sceneID)
+    swir2 = os.path.join(landsat_temp,"%s_sr_band7.swir2.dat" % sceneID)
+    cloud = os.path.join(landsat_temp,"%s_cfmask.cloud.dat" % sceneID)
+    #meta = landsat_metadata(os.path.join(landsat_temp,'%s_MTL.txt' % sceneID))
     sw_res = meta.GRID_CELL_SIZE_REFLECTIVE
     ulx = meta.CORNER_UL_PROJECTION_X_PRODUCT-(sw_res*0.5)
     uly = meta.CORNER_UL_PROJECTION_Y_PRODUCT+(sw_res*0.5)
@@ -115,9 +115,9 @@ def finalDMSinp(productID,ext):
     else:
         native_Thres = 90
     #filestem = os.path.join(landsatLAI,"lndsr_modlai_samples.combined_%s-%s" %(startDate,endDate))
-    lstFN = os.path.join(landsatTemp,"lndsr.%s.cband6.bin" % sceneID)
-    sharpendFN = os.path.join(landsatTemp,"%s.sharpened_band6.%s" % (sceneID,ext))
-    fn = os.path.join(landsatTemp,"dms.inp")
+    lstFN = os.path.join(landsat_temp,"lndsr.%s.cband6.bin" % sceneID)
+    sharpendFN = os.path.join(landsat_temp,"%s.sharpened_band6.%s" % (sceneID,ext))
+    fn = os.path.join(landsat_temp,"dms.inp")
     fn = "dms.inp"
     file = open(fn, "w")
     file.write("# input file for Data Mining Sharpener\n")
@@ -171,7 +171,7 @@ def localPred(productID,th_res,s_row,s_col):
     oe_row = e_row +overlap
     oe_col = e_col + overlap
     perpareDMSinp(productID,s_row,s_col,"local","bin")
-    #dmsfn = os.path.join(landsatTemp,"dms_%d_%d.inp" % (s_row,s_col))
+    #dmsfn = os.path.join(landsat_temp,"dms_%d_%d.inp" % (s_row,s_col))
     dmsfn = "dms_%d_%d.inp" % (s_row,s_col)
     # do cubist prediction
     subprocess.call(["get_samples","%s" % dmsfn,"%d" % os_row,"%d" % os_col,
@@ -195,12 +195,12 @@ def globalPredSK(sceneID):
     y = np.array(df.iloc[:,-2])
     regr_1.fit(X,y)
     regr_2.fit(X,y)
-    blue = os.path.join(landsatTemp,"%s_sr_band2.tif" % sceneID)
-    green = os.path.join(landsatTemp,"%s_sr_band3.tif" % sceneID)
-    red = os.path.join(landsatTemp,"%s_sr_band4.tif" % sceneID)
-    nir = os.path.join(landsatTemp,"%s_sr_band5.tif" % sceneID)
-    swir1 = os.path.join(landsatTemp,"%s_sr_band6.tif" % sceneID)
-    swir2 = os.path.join(landsatTemp,"%s_sr_band7.tif" % sceneID)
+    blue = os.path.join(landsat_temp,"%s_sr_band2.tif" % sceneID)
+    green = os.path.join(landsat_temp,"%s_sr_band3.tif" % sceneID)
+    red = os.path.join(landsat_temp,"%s_sr_band4.tif" % sceneID)
+    nir = os.path.join(landsat_temp,"%s_sr_band5.tif" % sceneID)
+    swir1 = os.path.join(landsat_temp,"%s_sr_band6.tif" % sceneID)
+    swir2 = os.path.join(landsat_temp,"%s_sr_band7.tif" % sceneID)
     # open files and assepble them into 2-d numpy array
     
     Gblue = gdal.Open(blue)
@@ -268,7 +268,7 @@ def localPredSK(sceneID,th_res,s_row,s_col):
     oe_row = e_row +overlap
     oe_col = e_col + overlap
     perpareDMSinp(sceneID,s_row,s_col,"local","bin")
-    #dmsfn = os.path.join(landsatTemp,"dms_%d_%d.inp" % (s_row,s_col))
+    #dmsfn = os.path.join(landsat_temp,"dms_%d_%d.inp" % (s_row,s_col))
     dmsfn = "dms_%d_%d.inp" % (s_row,s_col)
     # do cubist prediction
     subprocess.call(["get_samples","%s" % dmsfn,"%d" % os_row,"%d" % os_col,
@@ -288,7 +288,7 @@ def getSharpenedLST(metaFN):
     uly = meta.CORNER_UL_PROJECTION_Y_PRODUCT+(sw_res*0.5)
     xres = meta.GRID_CELL_SIZE_REFLECTIVE
     yres = meta.GRID_CELL_SIZE_REFLECTIVE   
-    ls = GeoTIFF(os.path.join(landsatTemp,'%s_sr_band1.tif' % productID))
+    ls = GeoTIFF(os.path.join(landsat_temp,'%s_sr_band1.tif' % productID))
     th_res = meta.GRID_CELL_SIZE_THERMAL
     if sceneID[2]=="5":
         th_res = 120
@@ -299,7 +299,7 @@ def getSharpenedLST(metaFN):
     scale = int(th_res/meta.GRID_CELL_SIZE_REFLECTIVE)
     nrows = int(meta.REFLECTIVE_LINES/scale)
     ncols = int(meta.REFLECTIVE_SAMPLES/scale)
-    #dmsfn = os.path.join(landsatTemp,"dms_0_0.inp")
+    #dmsfn = os.path.join(landsat_temp,"dms_0_0.inp")
     dmsfn = "dms.inp"
     # create dms.inp
     print("========GLOBAL PREDICTION===========")
@@ -313,7 +313,7 @@ def getSharpenedLST(metaFN):
         subprocess.call(["predict_fineT","%s" % dmsfn])
     else:
         #===========EXPERIMENTAL===========
-        globFN = os.path.join(landsatTemp,"%s.sharpened_band6.global" % sceneID)
+        globFN = os.path.join(landsat_temp,"%s.sharpened_band6.global" % sceneID)
         globalData = globalPredSK(sceneID)
         writeArray2Envi(globalData,ulx,uly,xres,yres,ls.proj4,globFN)
     # do local prediction
@@ -324,25 +324,25 @@ def getSharpenedLST(metaFN):
     # process local parts in parallel
     Parallel(n_jobs=njobs, verbose=5)(delayed(localPred)(productID,th_res,s_row,s_col) for s_col in range(0,int(ncols/wsize)*wsize,wsize) for s_row in range(0,int(nrows/wsize)*wsize,wsize))
     # put the parts back together
-    finalFile = os.path.join(landsatTemp,'%s.sharpened_band6.local' % sceneID)
-    tifFile = os.path.join(landsatTemp,'%s_lstSharp.tiff' % sceneID)
-    globFN = os.path.join(landsatTemp,"%s.sharpened_band6.global" % sceneID)
+    finalFile = os.path.join(landsat_temp,'%s.sharpened_band6.local' % sceneID)
+    tifFile = os.path.join(landsat_temp,'%s_lstSharp.tiff' % sceneID)
+    globFN = os.path.join(landsat_temp,"%s.sharpened_band6.global" % sceneID)
     Gg = gdal.Open(globFN)
     globalData = Gg.ReadAsArray()
     for s_col in range(0,int(ncols/wsize)*wsize,wsize): 
         for s_row in range(0,int(nrows/wsize)*wsize,wsize):
-            fn = os.path.join(landsatTemp,"%s.local_sharpened_%d_%d.bin" %(sceneID,s_row,s_col))
+            fn = os.path.join(landsat_temp,"%s.local_sharpened_%d_%d.bin" %(sceneID,s_row,s_col))
             if os.path.exists(fn):
                 Lg = gdal.Open(fn)
                 globalData[0,s_row*scale:s_row*scale+wsize*scale+1,s_col*scale:s_col*scale+wsize*scale+1] = Lg.ReadAsArray(s_col*scale,s_row*scale,wsize*scale+1,wsize*scale+1)[0]
     writeArray2Envi(globalData,ulx,uly,xres,yres,ls.proj4,finalFile)
       
-    #subprocess.call(["gdal_merge.py", "-o", "%s" % finalFile , "%s" % os.path.join(landsatTemp,'%s.local*' % sceneID)])
+    #subprocess.call(["gdal_merge.py", "-o", "%s" % finalFile , "%s" % os.path.join(landsat_temp,'%s.local*' % sceneID)])
     # combine the the local and global images
     finalDMSinp(productID,"bin")
     subprocess.call(["combine_models","dms.inp"])
     # convert from ENVI to geoTIFF
-    fn = os.path.join(landsatTemp,"%s.sharpened_band6.bin" % sceneID)
+    fn = os.path.join(landsat_temp,"%s.sharpened_band6.bin" % sceneID)
     g = gdal.Open(fn)
     #=====convert to celcius and scale data======
     data = g.ReadAsArray()[1]
@@ -351,14 +351,14 @@ def getSharpenedLST(metaFN):
     ls.clone(tifFile,dd)
     
     # copy files to their proper places
-    scenePath = os.path.join(landsatLST,sceneID[3:9])
+    scenePath = os.path.join(landsat_LST,sceneID[3:9])
     if not os.path.exists(scenePath):
         os.mkdir(scenePath)
     shutil.copyfile(tifFile ,os.path.join(scenePath,tifFile.split(os.sep)[-1]))
     
     # cleaning up    
-    clean(landsatTemp,"%s.local_sharpened" % sceneID)
-    clean(landsatTemp,"%s.sharpened" % sceneID)
+    clean(landsat_temp,"%s.local_sharpened" % sceneID)
+    clean(landsat_temp,"%s.sharpened" % sceneID)
     clean(base,"th_samples")
     clean(base,"dms")
     print("DONE SHARPENING")
