36aa6dfcd1de66a59f861bbc63902a2c0b6aa7db
bucricket <bucricket@gmail.com>
2017-05-06 17:49:33 -0400

    Changed setup.py

--- setup.py
+++ setup.py
@@ -66,9 +66,9 @@ setup(
     author="Mitchell Schull",
     author_email="mitch.schull@noaa.gov",
     url="https://github.com/bucricket/projectMASlst.git",
-    py_modules=['processlst.processlst','processlst.utils',
-                'processlst.lndlst_dms','processlst.landsatTools',
-                'processlst.processData'],
+    py_modules=['processlst','utils',
+                'lndlst_dms','landsatTools',
+                'processData'],
     platforms='Posix; MacOS X; Windows',
     license='BSD 3-Clause',
     classifiers=[
